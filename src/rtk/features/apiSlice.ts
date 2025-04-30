ed5dc88535757c300a8db2b06fb3d255c5078297
ukwuani <ukwuanibarnabas@gmail.com>
2025-04-30 07:07:11 -0700

    Add base api slice

---
+++ src/rtk/features/apiSlice.ts
@@ -0,0 +1,33 @@
+
+// Import the RTK Query methods from the React-specific entry point
+import { retry, createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+import { baseurl } from '../api.constants';
+
+
+// Create our baseQuery instance
+const baseQuery = fetchBaseQuery({
+    baseUrl: baseurl, //!::anti-pattern: no env file setup yet
+    prepareHeaders: (headers, { getState }) => {
+      // By default, if we have a token in the store, let's use that for authenticated requests
+    //   const token = (getState() as RootState).users.token;
+    //   if (token) {
+    //     headers.set("Authorization", `Bearer ${token}`);  
+    //   }
+    //   return headers;
+    },
+  });
+
+const baseQueryWithRetry = retry(baseQuery, { maxRetries: 6 });
+
+// Define our single API slice object
+export const apiSlice = createApi({
+  // The cache reducer expects to be added at `state.api` (already default - this is optional)
+  reducerPath: 'api',
+  // All of our requests will have URLs starting with '/fakeApi'
+  baseQuery: baseQueryWithRetry,
+  endpoints: () =>({})
+})
+
+// Export the auto-generated hook for the `getPosts` query endpoint
+// export const { useGetPostsQuery } = apiSlice
