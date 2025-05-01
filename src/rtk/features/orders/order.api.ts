ae1c0282b1dd72a8cabc727dba5e61b76f1f145a
ukwuani <ukwuanibarnabas@gmail.com>
2025-05-01 08:25:40 -0700

    Add order slice rtk

---
+++ src/rtk/features/orders/order.api.ts
@@ -0,0 +1,45 @@
+import API from "src/rtk/api.constants";
+
+import { apiSlice } from "../apiSlice";
+
+import type { User } from "./order.dto";
+
+
+
+export const userApiSlice = apiSlice.injectEndpoints({
+    endpoints: (build) => ({
+        login: build.mutation<any, Partial<User>>({
+            query(body) {
+              return {
+                url: API.LOGIN,
+                method: 'POST',
+                body,
+              }
+            },
+            extraOptions: {
+              maxRetries: 1,
+            },
+            invalidatesTags: ['user'],
+          }),
+          getAllUsers: build.query<{data:  User[] }, void>({
+            query() {
+              return {
+                url: API.FETCH_USERS,
+              }
+            },
+            providesTags: ["user"]
+          }),
+          // getMessages: build.query<{ data: IConversation }, string>({
+          //   query(conversationId) {
+          //     return {
+          //       url: API.CHAT_MESSAGES(conversationId),
+          //     }
+          //   },
+          //   providesTags: ["message"]
+          // }),
+          
+    })
+})
+
+
+export const { useLoginMutation, useGetAllUsersQuery, useLazyGetAllUsersQuery } = userApiSlice;
---
+++ src/rtk/features/orders/orderSlice.ts
@@ -0,0 +1,39 @@
+import { createEntityAdapter } from '@reduxjs/toolkit'
+
+import { createAppSlice } from '../../hooks'
+
+import type { User } from './order.dto'
+
+// export const fetchUsers = createAppAsyncThunk('users/fetchUsers', async () => {
+//   const response = await client.get<User[]>(API.FETCH_USERS)
+//   return response.data
+// })
+
+const usersAdapter = createEntityAdapter<User>()
+
+const initialState = usersAdapter.getInitialState()
+
+
+const usersSlice = createAppSlice({
+  name: 'users',
+  initialState,
+  reducers: {},
+  // extraReducers(builder) {
+  //   builder.addCase(fetchUsers.fulfilled, usersAdapter.setAll)
+  // },
+})
+
+export default usersSlice
+
+// export const { selectAll: selectAllUsers, selectById: selectUserById } = usersAdapter.getSelectors(
+//   (state: RootState) => state.users,
+// )
+
+// export const selectCurrentUser = (state: RootState) => {
+//   const currentUsername = 
+//   if (!currentUsername) {
+//     return
+//   }
+
+//   return selectUserById(state, currentUsername)
+// }
