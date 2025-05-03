e83b715d04ef27b96f8e23e88b53b679b67172f2
ukwuani <ukwuanibarnabas@gmail.com>
2025-05-02 20:10:32 -0700

    Remove alert box

--- src/layouts/auth/layout.tsx
+++ src/layouts/auth/layout.tsx
@@ -54,15 +54,7 @@ export function AuthLayout({
           {/** @slot Logo */}
           <Logo />
         </>
-      ),
-      rightArea: (
-        <Box sx={{ display: 'flex', alignItems: 'center', gap: { xs: 1, sm: 1.5 } }}>
-          {/** @slot Help link */}
-          <Link href="#" component={RouterLink} color="inherit" sx={{ typography: 'subtitle2' }}>
-            Need help?
-          </Link>
-        </Box>
-      ),
+      )
     };
 
     return (
