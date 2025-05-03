b042c55547f16ea651e5dbf19d11bb67715a7ff9
ukwuani <ukwuanibarnabas@gmail.com>
2025-05-02 20:10:07 -0700

    Adjust language

--- src/_mock/_data.ts
+++ src/_mock/_data.ts
@@ -103,17 +103,7 @@ export const _langs = [
     value: 'en',
     label: 'English',
     icon: '/assets/icons/flags/ic-flag-en.svg',
-  },
-  {
-    value: 'de',
-    label: 'German',
-    icon: '/assets/icons/flags/ic-flag-de.svg',
-  },
-  {
-    value: 'fr',
-    label: 'French',
-    icon: '/assets/icons/flags/ic-flag-fr.svg',
-  },
+  }
 ];
 
 // ----------------------------------------------------------------------
