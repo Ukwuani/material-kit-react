567ccc682348e52cda879e1fd7690fd4f8f8ed1b
ukwuani <ukwuanibarnabas@gmail.com>
2025-05-16 12:59:54 -0700

    Adjust products to slice

--- src/rtk/features/products/productSlice.ts
+++ src/rtk/features/products/productSlice.ts
@@ -4,11 +4,6 @@ import { createAppSlice } from '../../hooks'
 
 import type { Product } from './product.dto'
 
-// export const fetchUsers = createAppAsyncThunk('users/fetchUsers', async () => {
-//   const response = await client.get<User[]>(API.FETCH_USERS)
-//   return response.data
-// })
-
 const productAdapter = createEntityAdapter<Product>()
 
 const initialState = productAdapter.getInitialState()
@@ -17,10 +12,7 @@ const initialState = productAdapter.getInitialState()
 const usersSlice = createAppSlice({
   name: 'product',
   initialState,
-  reducers: {},
-  // extraReducers(builder) {
-  //   builder.addCase(fetchUsers.fulfilled, usersAdapter.setAll)
-  // },
+  reducers: {}
 })
 
 export default usersSlice
